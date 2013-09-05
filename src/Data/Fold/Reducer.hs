f674a4aa04745b068157e61cd2d292d9bd950280
Edward Kmett <ekmett@gmail.com>
2013-09-05 14:57:11 -0400

    Switch to a 'Traced'-based 'Comonad' for Reducer. The old one was unsound.

--- src/Data/Fold/Reducer.hs
+++ src/Data/Fold/Reducer.hs
@@ -11,41 +11,28 @@ module Data.Fold.Reducer
 import Control.Applicative
 import Control.Comonad
 import Control.Lens
-import Data.Fold.Class
-import Data.Foldable hiding (sum, product)
+-- import Data.Fold.Class
+-- import Data.Foldable hiding (sum, product)
 import Data.Functor.Extend
 import Data.Functor.Apply
 import Data.Monoid
 import Data.Profunctor.Unsafe
 import Data.Proxy
 import Data.Reflection
-import Data.Traversable
+-- import Data.Traversable
 import Unsafe.Coerce
 import Prelude hiding (sum, product, length)
 
 -- sequence algebras
 data Reducer b a = forall m. Reducer (m -> a) (b -> m) (m -> m -> m) m
 
-newtype M a s = M { runM :: a }
+newtype M a s = M a
 
 instance Reifies s (a -> a -> a, a) => Monoid (M a s) where
   mempty = M $ snd $ reflect (Proxy :: Proxy s)
   {-# INLINE mempty #-}
   mappend (M a) (M b) = M $ fst (reflect (Proxy :: Proxy s)) a b
   {-# INLINE mappend #-}
-
-instance Folding Reducer where
-  enfold tb (Reducer k h m (e :: m)) = reify (m,e) $
-    \ (_ :: Proxy s) -> k $ runM (foldMap (M #. h) tb :: M m s)
-
-  enfoldOf l s (Reducer k h m (e :: m)) = reify (m,e) $
-    \ (_ :: Proxy s) -> k $ runM (foldMapOf l (M #. h) s :: M m s)
-
-  enscan s (Reducer k h m z) = snd (mapAccumL h' z s) where
-    h' r a = (r', k r') where r' = m r (h a)
-
-  enscanOf l s (Reducer k h m z) = snd (mapAccumLOf l h' z s) where
-    h' r a = (r', k r') where r' = m r (h a)
 
 instance Profunctor Reducer where
   dimap f g (Reducer k h m e) = Reducer (g.k) (h.f) m e
@@ -80,14 +67,11 @@ instance Functor (Reducer a) where
   {-# INLINE (<$) #-}
 
 instance Comonad (Reducer b) where
-  extract (Reducer k _ _ x) = k x
+  extract (Reducer k _ _ z) = k z
   {-# INLINE extract #-}
 
-  duplicate (Reducer k h m x) = Reducer (Reducer k h m) h m x
+  duplicate (Reducer k h m z) = Reducer (\n -> Reducer (k . m n) h m z) h m z
   {-# INLINE duplicate #-}
-
-  extend f (Reducer k h m x)  = Reducer (f . Reducer k h m) h m x
-  {-# INLINE extend #-}
 
 data Pair a b = Pair !a !b
 
