b7af42f0ac0b757068f27a178eaec5fb98ba32ba
Edward Kmett <ekmett@gmail.com>
2013-09-06 00:38:21 -0400

    added description for adler32

--- src/Data/Hash/Adler32.hs
+++ src/Data/Hash/Adler32.hs
@@ -8,6 +8,9 @@ import Data.Word
 
 data Adler32 = Adler32 {-# UNPACK #-} !Word32 {-# UNPACK #-} !Word32
 
+-- | <http://en.wikipedia.org/wiki/Adler-32 Adler-32> is a modified Fletcher checksum
+-- used in <http://en.wikipedia.org/wiki/Zlib zlib> designed by
+-- <http://en.wikipedia.org/wiki/Mark_Adler Mark Adler>.
 adler32 :: L' Word8 Word32
 adler32 = L' done step (Adler32 1 0) where
   step (Adler32 s1 s2) x = Adler32 s1' s2' where
