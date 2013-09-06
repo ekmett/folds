6db0e53c3281b95975a12d22b15e30abaa8abff8
Edward Kmett <ekmett@gmail.com>
2013-09-06 00:31:46 -0400

    added Data.Hash.Adler32

---
+++ src/Data/Hash/Adler32.hs
@@ -0,0 +1,17 @@
+module Data.Hash.Adler32
+  ( adler32
+  ) where
+
+import Data.Bits
+import Data.Fold.L'
+import Data.Word
+
+data Adler32 = Adler32 {-# UNPACK #-} !Word32 {-# UNPACK #-} !Word32
+
+adler32 :: L' Word8 Word32
+adler32 = L' done step (Adler32 1 0) where
+  step (Adler32 s1 s2) x = Adler32 s1' s2' where
+    s1' = mod (s1 + fromIntegral x) 65521
+    s2' = mod (s1' + s2) 65521
+  done (Adler32 s1 s2) = unsafeShiftL s2 16 + s1
+{-# INLINE adler32 #-}
