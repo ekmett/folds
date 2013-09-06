4cca5700b03f6c4abcfc70f1837ce0b30bf2a51f
Edward Kmett <ekmett@gmail.com>
2013-09-05 21:09:44 -0400

    Defined folding homomorphisms: r2m, m2r, l2m

--- src/Data/Fold.hs
+++ src/Data/Fold.hs
@@ -3,9 +3,39 @@ module Data.Fold
   , L(..)
   , M(..)
   , R(..)
+  , r2m
+  , m2r
+  , l2m
   ) where
 
 import Data.Fold.Class
 import Data.Fold.L
 import Data.Fold.M
 import Data.Fold.R
+import Control.Category ((>>>))
+
+-- |
+--
+-- 'foldr' and 'foldMap' are equivalent in expressive power for finite folds.
+r2m :: R a b -> M a b
+r2m (R k h z) = M (\f -> k (f z)) h (.) id
+
+-- |
+--
+-- We can convert from a monoidal fold to a right fold.
+--
+-- @
+-- run xs (m2r l) = run xs l
+-- @
+m2r :: M a b -> R a b
+m2r (M k h m z) = R k (m . h) z
+
+-- |
+--
+-- We can convert from a left fold to a monoidal fold.
+--
+-- @
+-- run xs (l2m l) = run xs l
+-- @
+l2m :: L a b -> M a b
+l2m (L k h z) = M (\f -> k (f z)) (flip h) (>>>) id
